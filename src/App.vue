6de18dea554b0f53037c7448d0e5acdcb4206529
Sakuto <vigneronlucweb@gmail.com>
2026-09-09 22:02:18 +0200

    Feature - Add patient space (médecin search, booking, RDV history and calendar)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { fr } from '@nuxt/ui/locale'
+</script>
 
 <template>
-  <UApp>
+  <UApp :locale="fr">
     <router-view />
   </UApp>
 </template>
---
+++ src/types.ts
@@ -0,0 +1,28 @@
+export interface Medecin {
+  id: number
+  nom: string
+  prenom: string
+  specialite_id: number
+  specialite_nom: string
+}
+
+export interface Specialite {
+  id: number
+  nom: string
+}
+
+export interface Creneau {
+  date_heure: string
+  disponible: boolean
+}
+
+export interface RendezVous {
+  id: number
+  patient_id: number
+  medecin_id: number
+  date_heure: string
+  statut: 'confirme' | 'annule' | 'honore'
+  medecin_nom: string
+  medecin_prenom: string
+  specialite_nom: string
+}
